570eb4aeca994d3b8624bb13698485121804f922
Riccardo Petraglia <riccardo.petraglia@epfl.ch>
2014-07-25 12:53:01 +0200

    Add: mopac-d3h4 calculator

---
+++ ase/calculators/dftbd3h4.py
@@ -0,0 +1,72 @@
+import os
+
+import numpy as np
+
+#from ase.calculators.calculator import FileIOCalculator, kpts2mp
+from ase.calculators.dftb import Dftb
+from ase.calculators.d3h4 import D3H4
+
+from ase.calculators.calculator import FileIOCalculator, kpts2mp
+
+class DftbD3H4(FileIOCalculator):
+    """ A dftb+-D3H4 calculator with ase-FileIOCalculator nomenclature
+    """
+
+    implemented_properties = ['energy', 'forces']#, 'charges']
+
+    def __init__(self, label='dftbddmc', atoms=None, dftbdict={}, d3h4dict={}, **kwargs):
+        """Construct a DFTB+ and a D3H4 calculator.
+        """
+
+        os.environ['ASE_DFTBD3H4_COMMAND'] = ''
+
+        self.label = label
+        self.dftbdict = dftbdict
+        self.d3h4dict = d3h4dict
+
+        restart = None
+        ignore_bad_restart_file = None
+        FileIOCalculator.__init__(self, restart, ignore_bad_restart_file,
+                                  label, atoms, **kwargs)
+
+        self.dftb_calc = Dftb(label=self.label)
+        self.d3h4_calc = D3H4(label=self.label)
+
+
+    def write_input(self, atoms, properties=None, systems_changes=None):
+        self.dftb_calc.parameters.update(self.dftbdict)
+        self.d3h4_calc.parameters.update(self.d3h4dict)
+
+        
+    def read_results(self):
+        self.read_energy()
+        self.read_forces()
+        
+    def read_energy(self):
+        results=[]
+        """Read Energy from dftb output file (results.tag)."""
+        from ase.units import Hartree
+        # Energy:
+        try:
+            self.atoms.set_calculator(self.dftb_calc)
+            dftb_energy = self.atoms.get_potential_energy()
+            self.atoms.set_calculator(self.d3h4_calc)
+            d3h4_energy = self.atoms.get_potential_energy()
+            self.results['energy'] = d3h4_energy+dftb_energy
+        except:
+            raise RuntimeError('Problem in reading energy')
+
+    def read_forces(self):
+        """Read Forces from dftb output file (results.tag)."""
+        from ase.units import Hartree, Bohr
+
+        try:
+            self.atoms.set_calculator(self.dftb_calc)
+            dftb_forces = self.atoms.get_forces()
+            self.atoms.set_calculator(self.d3h4_calc)
+            d3h4_forces = self.atoms.get_forces()
+            
+            self.results['forces'] = dftb_forces+d3h4_forces
+
+        except:
+            raise RuntimeError('Problem in reading forces')
